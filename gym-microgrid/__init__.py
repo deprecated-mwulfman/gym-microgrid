14c4a4ec09ca5c350a4a5a692c7b11d74c7098ca
mwulfman <45192866+mwulfman@users.noreply.github.com>
2019-11-01 17:48:44 -0700

    Update __init__.py

--- gym-microgrid/__init__.py
+++ gym-microgrid/__init__.py
@@ -1,1 +1,10 @@
+from gym.envs.registration import register
 
+register(
+    id='microgrid-v0',
+    entry_point='gym_microgrid.envs:MicrogridEnv',
+)
+register(
+    id='microgrid-extrahard-v0',
+    entry_point='gym_microgrid.envs:MicrogridExtraHardEnv',
+)
